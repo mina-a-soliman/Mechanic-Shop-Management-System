1280bbad6c59c35e4097188fb88ab8ef2ce86528
mina-a-soliman <minaashrafmoawad@gmail.com>
2026-07-04 23:58:36 +0300

    feat(domain): add application constants

---
+++ src/MechanicShop.Domain/Common/Constants/MechanicShopConstants.cs
@@ -0,0 +1,7 @@
+namespace MechanicShop.Domain.Common.Constants;
+
+public static class MechanicShopConstants
+{
+    public const decimal TaxRate = 0.15m;
+    public const string SystemUser = "System";
+}
