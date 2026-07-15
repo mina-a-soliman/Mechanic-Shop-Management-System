7da458e6d8cdecfcf89950936b72c90c69c7d49f
mina <minaashrafmoawad@gmail.com>
2026-07-15 22:54:41 +0300

    feat: add IAppDbContext abstraction

---
+++ src/MechanicShop.Application/Common/Interfaces/IAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+namespace MechanicShop.Application.Common.Interfaces;
+public interface IAppDbContext
+{
+
+    public DbSet<RefreshToken> RefreshTokens { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
