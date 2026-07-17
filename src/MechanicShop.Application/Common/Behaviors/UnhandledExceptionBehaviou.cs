6b782778b4706e452cd4c8b3b92faa1373b65e69
mina-a-soliman <minaashrafmoawad@gmail.com>
2026-07-18 01:57:48 +0300

    feat(application): introduce unhandled exception pipeline behavior

---
+++ src/MechanicShop.Application/Common/Behaviors/UnhandledExceptionBehaviou.cs
@@ -0,0 +1,28 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace MechanicShop.Application.Common.Behaviors;
+
+public class UnhandledExceptionBehavior<TRequest, TResponse>
+    (ILogger<TRequest> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+where TRequest : notnull
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            var requestName = typeof(TRequest).Name;
+
+            logger.LogError(ex,
+              "Request: unhandled Exception for Request {Name} {@Request}",
+              requestName, request);
+
+            throw;
+        }
+    }
+}
